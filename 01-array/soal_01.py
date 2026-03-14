fb5ff3464bd553b6f71eb198ae5cc1b0df978244
naufalqy <naufalqy11@gmail.com>
2026-03-14 15:54:34 +0800

    soal_01.py

---
+++ 01-array/soal_01.py
@@ -0,0 +1,27 @@
+
+nilai = []
+
+print("Masukkan 10 nilai siswa:")
+
+for i in range(10):
+    n = int(input(f"Nilai ke-{i+1}: "))
+    nilai.append(n)
+
+nilai_tertinggi = max(nilai)
+nilai_terendah = min(nilai)
+
+rata_rata = sum(nilai) / len(nilai)
+
+lulus = 0
+for n in nilai:
+    if n >= 60:
+        lulus += 1
+
+tidak_lulus = len(nilai) - lulus
+
+print("\n===== HASIL =====")
+print("Nilai tertinggi :", nilai_tertinggi)
+print("Nilai terendah :", nilai_terendah)
+print("Rata-rata :", rata_rata)
+print("Jumlah lulus :", lulus)
+print("Jumlah tidak lulus :", tidak_lulus)

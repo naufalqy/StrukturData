11e47040c09e912e0fbab294a6f210622abc0af4
naufalqy <naufalqy11@gmail.com>
2026-03-14 15:57:10 +0800

    soal_02.py

---
+++ 01-array/01-array/soal_02.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+nilai = []
+
+print("Masukkan 10 nilai siswa:")
+for i in range(10):
+    n = int(input(f"Nilai ke-{i+1}: "))
+    nilai.append(n)
+
+nilai_tertinggi = max(nilai)
+nilai_terendah = min(nilai)
+
+lulus = 0
+for n in nilai:
+    if n >= 60:
+        lulus += 1
+
+tidak_lulus = len(nilai) - lulus
+
+plt.bar(["Tertinggi","Terendah"],[nilai_tertinggi,nilai_terendah])
+plt.title("Perbandingan Nilai")
+plt.show()
+
+plt.bar(["Lulus","Tidak Lulus"],[lulus,tidak_lulus])
+plt.title("Data Kelulusan")
+plt.show()
